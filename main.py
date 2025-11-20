23ff346c8afe87f6265210db4988bb3b4679030b
MUHAMMAD HARIS KAMRAN <harixmughal65@gmail.com>
2025-11-20 19:15:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -146,9 +146,7 @@ def dashboard(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(
-        "sr_model.fusion_api:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
-        reload=True
-    )
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
