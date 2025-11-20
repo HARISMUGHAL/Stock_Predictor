cfb9d18dc87e63d77f866677ce13e918338f5f4f
MUHAMMAD HARIS KAMRAN <harixmughal65@gmail.com>
2025-11-20 19:48:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -146,4 +146,5 @@ def dashboard(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  #
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  # only for local testing
+
